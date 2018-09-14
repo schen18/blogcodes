f6c4e7f5c4244c393f4dc2f8fcca50fd0e4ebdd0
Stephen Chen <40239582+schen18@users.noreply.github.com>
2018-09-14 12:14:30 -0400

    Add files via upload

---
+++ correlations/corr1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+"""
+Set parameters below
+--------------------
+
+"""
+nsim=1000
+nsize=100
+nhigh=5
+
+"""
+nsim = number of simulations
+nsize = number of items in random arrays
+nhigh = highest integer in random arrays
+
+"""
+
+pcorr=[]
+pscorr=[]
+
+for s in range(nsim):
+    x=np.random.random_integers(1,nhigh,nsize)
+    y=np.random.random_integers(1,nhigh,nsize)
+    pcorr.append(np.corrcoef(x,y)[0,1])
+    # extracts first 30 items from x,y and calculates correlation
+    xs=x[:30] 
+    ys=y[:30] 
+    pscorr.append(np.corrcoef(xs,ys)[0,1])
+
+# Plot results as histograms
+plt.subplot(2, 1, 1)
+plt.hist(pcorr, bins = 20, color='g')
+plt.title('Number of random draws='+str(nsim)+' from 1 to '+str(nhigh))
+plt.ylabel('n='+str(nsize))
+
+plt.subplot(2, 1, 2)
+plt.hist(pscorr, bins = 20)
+plt.xlabel('Pearson Correlation')
+plt.ylabel('n=30')
+
+plt.show()
